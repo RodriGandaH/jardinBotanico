7f79eeab16f5713d2f44c17da99b3408e8f2c1a0
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2023-12-06 00:04:09 -0400

    Add Info component to admin section

--- jardin_FRONTEND/src/App.jsx
+++ jardin_FRONTEND/src/App.jsx
@@ -10,12 +10,13 @@ import useAuth from './components/admin/hooks/useAuth';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Galeria from './components/galeria/Galeria';
-import Planta from './components/planta/Planta'
+import Planta from './components/planta/Planta';
 import Eventos from './components/eventos/Eventos';
 import Category from './components/admin/category/Category';
 import Plants from './components/admin/plants/Plants';
 import Events from './components/admin/events/Events';
 import '../src/components/galeria/Galeria.css';
+import Info from './components/admin/info/Info';
 
 function App() {
     const auth = useAuth();
@@ -27,8 +28,14 @@ function App() {
     return (
         <>
             <Navbar isAdmin={auth.user} />
-            <main className='flex-shrink-0 container'
-                style={{ marginTop: '75px', marginBottom: '15px', minHeight: '91vh' }}>
+            <main
+                className="flex-shrink-0 container"
+                style={{
+                    marginTop: '75px',
+                    marginBottom: '15px',
+                    minHeight: '91vh',
+                }}
+            >
                 <Router>
                     <Routes>
                         <Route path="/" element={<Home />} />
@@ -36,9 +43,42 @@ function App() {
                         <Route path="/planta/:id" element={<Planta />} />
                         <Route path="/eventos" element={<Eventos />} />
                         <Route path="/login" element={<Login />} />
-                        <Route path="/admin/category" element={auth.user ? <Category /> : <Navigate to="/login" />} />
-                        <Route path="/admin/events" element={auth.user ? <Events /> : <Navigate to="/login" />} />
-                        <Route path="/admin/plants" element={auth.user ? <Plants /> : <Navigate to="/login" />} />
+                        <Route
+                            path="/admin/category"
+                            element={
+                                auth.user ? (
+                                    <Category />
+                                ) : (
+                                    <Navigate to="/login" />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/admin/events"
+                            element={
+                                auth.user ? (
+                                    <Events />
+                                ) : (
+                                    <Navigate to="/login" />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/admin/plants"
+                            element={
+                                auth.user ? (
+                                    <Plants />
+                                ) : (
+                                    <Navigate to="/login" />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/admin/info"
+                            element={
+                                auth.user ? <Info /> : <Navigate to="/info" />
+                            }
+                        />
                     </Routes>
                 </Router>
             </main>
--- jardin_FRONTEND/src/components/layout/Navbar.jsx
+++ jardin_FRONTEND/src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
-import LogoutButton from "../admin/LogoutButton";
+import LogoutButton from '../admin/LogoutButton';
 
 export const activeUrl = (ruta) => {
-    ruta = "http://localhost:5173" + ruta;
+    ruta = 'http://localhost:5173' + ruta;
     let rutaActiva = window.location.href;
     return ruta === rutaActiva;
 };
@@ -10,43 +10,96 @@ const Navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-lg fixed-top bg-success-subtle">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Jardín</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="/">
+                    Jardín
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item" >
+                        <li className="nav-item">
                             <a
-                                className={("nav-link" + (activeUrl("/galeria") ? " active" : ""))}
-                                href="/galeria">
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/galeria') ? ' active' : '')
+                                }
+                                href="/galeria"
+                            >
                                 Galería
                             </a>
                         </li>
                         <li className="nav-item">
                             <a
-                                className={"nav-link" + (activeUrl("/eventos") ? " active" : "")}
-                                href="/eventos">
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/eventos') ? ' active' : '')
+                                }
+                                href="/eventos"
+                            >
                                 Eventos
                             </a>
                         </li>
                         <div className="vr" hidden={!props.isAdmin}></div>
                         <li className="nav-item" hidden={!props.isAdmin}>
-                            <a className={"nav-link" + (activeUrl("/admin/category") ? " active" : "")}
-                                href="/admin/category">
+                            <a
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/admin/category')
+                                        ? ' active'
+                                        : '')
+                                }
+                                href="/admin/category"
+                            >
                                 A. Categorías
                             </a>
                         </li>
                         <li className="nav-item" hidden={!props.isAdmin}>
-                            <a className={"nav-link" + (activeUrl("/admin/plants") ? " active" : "")}
-                                href="/admin/plants">
+                            <a
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/admin/plants')
+                                        ? ' active'
+                                        : '')
+                                }
+                                href="/admin/plants"
+                            >
                                 A. Galería
                             </a>
                         </li>
                         <li className="nav-item" hidden={!props.isAdmin}>
-                            <a className={"nav-link" + (activeUrl("/admin/events") ? " active" : "")}
-                                href="/admin/events">
+                            <a
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/admin/events')
+                                        ? ' active'
+                                        : '')
+                                }
+                                href="/admin/events"
+                            >
                                 A. Eventos
+                            </a>
+                        </li>
+                        <li className="nav-item" hidden={!props.isAdmin}>
+                            <a
+                                className={
+                                    'nav-link' +
+                                    (activeUrl('/admin/info') ? ' active' : '')
+                                }
+                                href="/admin/info"
+                            >
+                                A. Informacion
                             </a>
                         </li>
                     </ul>
