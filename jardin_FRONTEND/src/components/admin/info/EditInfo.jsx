78f52e8dcccb28b5eeebe868c973e1728db87494
Jorge2610 <jorge261093@gmail.com>
2023-12-14 20:08:46 -0400

    Validacion en editar informacion

--- jardin_FRONTEND/src/components/admin/info/EditInfo.jsx
+++ jardin_FRONTEND/src/components/admin/info/EditInfo.jsx
@@ -5,71 +5,94 @@ function Editinf({ onUpdate, inf }) {
     const [name, setName] = useState(inf.name);
     const [data, setData] = useState(inf.data);
     const [label, setLabel] = useState('Dato');
+    const idForm = "idFormEditarInfo" + inf.id;
+    const idData = "idDataEditarInfo" + inf.id;
+    const idFeedback = "idFeedbackEditarInfo" + inf.id;
 
     useEffect(() => {
-        const modal = document.getElementById(`editinfModal-${inf.id}`);
-        $(modal).on('hidden.bs.modal', async function () {
-            const fetchData = async () => {
-                const token = localStorage.getItem('token');
-                const response = await axios.get(
-                    `http://127.0.0.1:8000/api/networks/${inf.id}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    }
-                );
-                setName(response.data.name);
-                setData(response.data.data);
-            };
-            fetchData();
-        });
-
-        return () => {
-            $(modal).off('hidden.bs.modal');
-        };
-    }, [inf.id]);
-
-    useEffect(() => {
+        let input = document.getElementById(idData);
+        let dataFeedback = document.getElementById(idFeedback);
         switch (name) {
             case 'Email':
+                input.setAttribute("type", "email")
+                input.setAttribute("pattern", ".+@.+\\..{2,4}$");
+                dataFeedback.innerText = "Correo electrónico no válido";
                 setLabel('Correo');
                 break;
             case 'Telefono':
+                input.setAttribute("type", "tel")
+                input.setAttribute("pattern", "[0-9]{7,9}");
+                dataFeedback.innerText = "Número telefónico no válido";
                 setLabel('Número');
                 break;
             case 'Facebook':
+                input.setAttribute("type", "url")
+                input.setAttribute("pattern", "https://www.facebook.com/.+");
+                dataFeedback.innerText = "Enlace a Facebook no válido.";
+                setLabel('URL');
+                break;
             case 'Instagram':
+                input.setAttribute("type", "url")
+                input.setAttribute("pattern", "https://www.instagram.com/.+");
+                dataFeedback.innerText = "Enlace a Instagram no válido.";
+                setLabel('URL');
+                break;
             case 'Twitter':
+                input.setAttribute("type", "url")
+                input.setAttribute("pattern", "https://twitter.com/.+");
+                dataFeedback.innerText = "Enlace a Twitter no válido.";
                 setLabel('URL');
                 break;
             default:
+                input.setAttribute("type", "text")
+                input.setAttribute("pattern", "");
+                dataFeedback.innerText = "";
                 setLabel('Dato');
         }
     }, [name]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
-        const token = localStorage.getItem('token');
-        try {
-            const response = await axios.put(
-                `http://127.0.0.1:8000/api/networks/${inf.id}`,
-                { name, data },
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-
-            console.log('infrmación actualizada:', { name, data });
-
-            $(`#editinfModal-${inf.id}`).modal('hide');
-            onUpdate();
-        } catch (error) {
-            console.log('Error al actualizar la infrmacion:', error);
+        let form = document.getElementById(idForm);
+        if (form.checkValidity()) {
+            const token = localStorage.getItem('token');
+            try {
+                const response = await axios.put(
+                    `http://127.0.0.1:8000/api/networks/${inf.id}`,
+                    { name, data },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                resetModal()
+                $(`#editinfModal-${inf.id}`).modal('hide');
+                onUpdate();
+            } catch (error) {
+                console.log('Error al actualizar la infrmacion:', error);
+            }
+        } else {
+            form.classList.add("was-validated");
         }
+    };
+
+    const resetModal = async () => {
+        document.getElementById(idForm).classList.remove("was-validated");
+        document.getElementById(idData).setAttribute("type", "text");
+        document.getElementById(idFeedback).setAttribute("pattern", "");
+        const token = localStorage.getItem('token');
+        const response = await axios.get(
+            `http://127.0.0.1:8000/api/networks/${inf.id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        setName(response.data.name);
+        setData(response.data.data);
+        setLabel('Dato');
     };
 
     return (
@@ -103,19 +126,22 @@ function Editinf({ onUpdate, inf }) {
                                 className="btn-close"
                                 data-bs-dismiss="modal"
                                 aria-label="close"
+                                onClick={resetModal}
                             ></button>
                         </div>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit} className='needs-validation' noValidate id={idForm}>
                             <div className="modal-body">
-                                <label htmlFor="name" className="form-label">
-                                    informacion:
+                                <label htmlFor={"inf" + inf.id} className="form-label">
+                                    Información:
                                 </label>
                                 <select
                                     name="inf"
-                                    id="inf"
+                                    id={"inf" + inf.id}
                                     className="form-select mb-3"
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
+                                    disabled
+                                    required
                                 >
                                     <option value="">
                                         Seleccione una opción
@@ -126,21 +152,30 @@ function Editinf({ onUpdate, inf }) {
                                     <option value="Telefono">Telefono</option>
                                     <option value="Twitter">Twitter</option>
                                 </select>
-                                <label htmlFor="url" className="form-label">
+                                <div className="invalid-feedback">
+                                    Debe seleccionar una opción valida.
+                                </div>
+                                <label htmlFor={idData} className="form-label">
                                     {label}:
                                 </label>
                                 <input
+                                    id={idData}
                                     type="text"
                                     className="form-control"
                                     value={data}
                                     onChange={(e) => setData(e.target.value)}
+                                    required
                                 />
+                                <div className="invalid-feedback" id={idFeedback}>
+
+                                </div>
                             </div>
                             <div className="modal-footer">
                                 <button
                                     type="button"
                                     className="btn btn-secondary"
                                     data-bs-dismiss="modal"
+                                    onClick={resetModal}
                                 >
                                     Cerrar
                                 </button>
